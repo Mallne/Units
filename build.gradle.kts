1a26fb28ba5a0f271f9220fb82610571dd11e0bb
Mallne <info@mallne.cloud>
2026-01-21 20:59:51 +0100

    Upgrade to Gradle 9.0 and update plugin dependencies
    Updated Gradle to version 9.0 and aligned plugin versions to their latest compatible releases, ensuring compatibility with the new Gradle version. Enhanced Kotlin Multiplatform configurations by introducing WASM and reorganizing Android library settings, improving support for modern platforms. Simplified and optimized the build setup for better maintainability and future development.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+@file:OptIn(ExperimentalWasmDsl::class)
+
+import com.android.build.api.dsl.androidLibrary
 import nl.littlerobots.vcu.plugin.resolver.VersionSelectors
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
@@ -27,12 +31,23 @@ version = "1.0.0-SNAPSHOT"
 
 kotlin {
     jvm()
-    androidTarget {
-        publishLibraryVariants("release")
+    androidLibrary {
+        namespace = project.group.toString()
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
         @OptIn(ExperimentalKotlinGradlePluginApi::class)
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_21)
         }
+    }
+    js {
+        nodejs()
+        browser()
+    }
+    wasmJs {
+        browser()
+        nodejs()
+        d8()
     }
     iosX64()
     iosArm64()
@@ -46,18 +61,6 @@ kotlin {
                 api(libs.kotlinx.serialization.json)
             }
         }
-    }
-}
-
-android {
-    namespace = "cloud.mallne"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
     }
 }
 
